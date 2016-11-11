a4caebf0cd926c86cd6df39fc7d1e90500f955d0
Tomasz Mazurek <Tomasz.A.Mazurek@gmail.com>
2016-11-11 20:37:37 +0100

    pycharm second commit

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'bookstore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^store/', include('store.urls')),
+    #url(r'^$', 'store.views.index', name='index'),
+    url(r'^store/', include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
